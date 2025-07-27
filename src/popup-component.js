0743cf5b94009978e0bac3c33c4bfa414f00690c
aelvanna <lmarie.fernandez@gmail.com>
2025-07-28 00:40:03 +1000

    Refactored share popup and updated styles: cleaned HTML structure, enhanced accessibility for social media links, and moved import font to CSS.

--- src/popup-component.js
+++ src/popup-component.js
@@ -1,12 +1,12 @@
 /* Function
- * When the share-button is clicked, the popup overlay with links to share
- * the article to social media opens. The author-info is replaced by the
- * overlay. Clicking the button again will close the popup.
- */
+ * When the share-button is clicked, open the popup overlay with sharing options.
+ * The author-info is replaced by the overlay. Clicking the button again will close the popup. */
 
+// Find the share button and the popup element.
 const shareButton = document.querySelector(".author-info__button");
 const sharePopup = document.getElementById("share-popup");
 
+// Add an event listener to the button to toggle the popup visibility.
 shareButton.addEventListener("click", () => {
   // Toggle the .active class on both the button and the popup.
   shareButton.classList.toggle("active");
